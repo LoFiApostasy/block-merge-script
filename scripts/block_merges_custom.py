b8719a34f77bb1e3fbbae9f46d903327b7a925d9
LoFiApostasy <123475841+LoFiApostasy@users.noreply.github.com>
2023-01-24 07:00:10 -0900

    Update block_merges_custom.py

--- scripts/block_merges_custom.py
+++ scripts/block_merges_custom.py
@@ -7,7 +7,7 @@ import math
 import random
 import gradio as gr
 import modules.scripts as scripts
-from gradio import interface as gr
+from gradio import interface
 from tqdm import tqdm
 from modules import sd_models
 from string import Template
