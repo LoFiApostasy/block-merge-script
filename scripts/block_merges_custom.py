c5f771f165ca3a352b929d71ab0eeba75e6a46f9
LoFiApostasy <123475841+LoFiApostasy@users.noreply.github.com>
2023-01-24 15:56:41 -0900

    Update block_merges_custom.py

--- scripts/block_merges_custom.py
+++ scripts/block_merges_custom.py
@@ -1,233 +1,59 @@
 import os
 import argparse
 import re
-import shutil
 import torch
-import safetensors.torch
 import math
 import random
-import collections
-from collections import namedtuple
 import gradio as gr
 import modules.scripts as scripts
-from gradio import interface as gr
 from tqdm import tqdm
 from modules import sd_models
 from string import Template
 from pathlib import Path
 from modules.processing import process_images, fix_seed, Processed
 from modules.shared import opts
-from modules.paths import models_path
 import modules.sd_models
-from modules import shared, sd_vae, images, sd_models, devices, script_callbacks
-from modules.ui_common import plaintext_to_html
+from modules import shared, sd_vae, images, sd_models #new code, last 2
 import copy
-import glob
-from copy import deepcopy
 
-base_vae = None
-loaded_vae_file = None
-checkpoint_info = None
-
-checkpoints_loaded = collections.OrderedDict()
-
-def get_base_vae(model):
-    if base_vae is not None and checkpoint_info == model.sd_checkpoint_info and model:
-        return base_vae
-    return None
-
-
-def store_base_vae(model):
-    global base_vae, checkpoint_info
-    if checkpoint_info != model.sd_checkpoint_info:
-        assert not loaded_vae_file, "Trying to store non-base VAE!"
-        base_vae = deepcopy(model.first_stage_model.state_dict())
-        checkpoint_info = model.sd_checkpoint_info
-
-
-def delete_base_vae():
-    global base_vae, checkpoint_info
-    base_vae = None
-    checkpoint_info = None
-
-
-def restore_base_vae(model):
-    global loaded_vae_file
-    if base_vae is not None and checkpoint_info == model.sd_checkpoint_info:
-        print("Restoring base VAE")
-        _load_vae_dict(model, base_vae)
-        loaded_vae_file = None
-    delete_base_vae()
-
-
-def get_filename(filepath):
-    return os.path.basename(filepath)
-
-
-def refresh_vae_list():
-    vae_dict.clear()
-
-    paths = [
-        os.path.join(sd_models.model_path, '**/*.vae.ckpt'),
-        os.path.join(sd_models.model_path, '**/*.vae.pt'),
-        os.path.join(sd_models.model_path, '**/*.vae.safetensors'),
-        os.path.join(vae_path, '**/*.ckpt'),
-        os.path.join(vae_path, '**/*.pt'),
-        os.path.join(vae_path, '**/*.safetensors'),
-    ]
-
-    if shared.cmd_opts.ckpt_dir is not None and os.path.isdir(shared.cmd_opts.ckpt_dir):
-        paths += [
-            os.path.join(shared.cmd_opts.ckpt_dir, '**/*.vae.ckpt'),
-            os.path.join(shared.cmd_opts.ckpt_dir, '**/*.vae.pt'),
-            os.path.join(shared.cmd_opts.ckpt_dir, '**/*.vae.safetensors'),
-        ]
-
-    if shared.cmd_opts.vae_dir is not None and os.path.isdir(shared.cmd_opts.vae_dir):
-        paths += [
-            os.path.join(shared.cmd_opts.vae_dir, '**/*.ckpt'),
-            os.path.join(shared.cmd_opts.vae_dir, '**/*.pt'),
-            os.path.join(shared.cmd_opts.vae_dir, '**/*.safetensors'),
-        ]
-
-    candidates = []
-    for path in paths:
-        candidates += glob.iglob(path, recursive=True)
-
-    for filepath in candidates:
-        name = get_filename(filepath)
-        vae_dict[name] = filepath
-
-
-def find_vae_near_checkpoint(checkpoint_file):
-    checkpoint_path = os.path.splitext(checkpoint_file)[0]
-    for vae_location in [checkpoint_path + ".vae.pt", checkpoint_path + ".vae.ckpt", checkpoint_path + ".vae.safetensors"]:
-        if os.path.isfile(vae_location):
-            return vae_location
-
-    return None
-
-
-def resolve_vae(checkpoint_file):
-    if shared.cmd_opts.vae_path is not None:
-        return shared.cmd_opts.vae_path, 'from commandline argument'
-
-    is_automatic = shared.opts.sd_vae in {"Automatic", "auto"}  # "auto" for people with old config
-
-    vae_near_checkpoint = find_vae_near_checkpoint(checkpoint_file)
-    if vae_near_checkpoint is not None and (shared.opts.sd_vae_as_default or is_automatic):
-        return vae_near_checkpoint, 'found near the checkpoint'
-
-    if shared.opts.sd_vae == "None":
-        return None, None
-
-    vae_from_options = vae_dict.get(shared.opts.sd_vae, None)
-    if vae_from_options is not None:
-        return vae_from_options, 'specified in settings'
-
-    if not is_automatic:
-        print(f"Couldn't find VAE named {shared.opts.sd_vae}; using None instead")
-
-    return None, None
-
-
-def load_vae_dict(filename, map_location):
-    vae_ckpt = sd_models.read_state_dict(filename, map_location=map_location)
-    vae_dict_1 = {k: v for k, v in vae_ckpt.items() if k[0:4] != "loss" and k not in vae_ignore_keys}
-    return vae_dict_1
-
-
-def load_vae(model, vae_file=None, vae_source="from unknown source"):
-    global vae_dict, loaded_vae_file
-    # save_settings = False
-
-    cache_enabled = shared.opts.sd_vae_checkpoint_cache > 0
-
-    if vae_file:
-        if cache_enabled and vae_file in checkpoints_loaded:
-            # use vae checkpoint cache
-            print(f"Loading VAE weights {vae_source}: cached {get_filename(vae_file)}")
-            store_base_vae(model)
-            _load_vae_dict(model, checkpoints_loaded[vae_file])
-        else:
-            assert os.path.isfile(vae_file), f"VAE {vae_source} doesn't exist: {vae_file}"
-            print(f"Loading VAE weights {vae_source}: {vae_file}")
-            store_base_vae(model)
-
-            vae_dict_1 = load_vae_dict(vae_file, map_location=shared.weight_load_location)
-            _load_vae_dict(model, vae_dict_1)
-
-            if cache_enabled:
-                # cache newly loaded vae
-                checkpoints_loaded[vae_file] = vae_dict_1.copy()
-
-        # clean up cache if limit is reached
-        if cache_enabled:
-            while len(checkpoints_loaded) > shared.opts.sd_vae_checkpoint_cache + 1: # we need to count the current model
-                checkpoints_loaded.popitem(last=False)  # LRU
-
-        # If vae used is not in dict, update it
-        # It will be removed on refresh though
-        vae_opt = get_filename(vae_file)
-        if vae_opt not in vae_dict:
-            vae_dict[vae_opt] = vae_file
-
-    elif loaded_vae_file:
-        restore_base_vae(model)
-
-    loaded_vae_file = vae_file
-
-
-# don't call this from outside
-def _load_vae_dict(model, vae_dict_1):
-    model.first_stage_model.load_state_dict(vae_dict_1)
-    model.first_stage_model.to(devices.dtype_vae)
-
-
-def clear_loaded_vae():
-    global loaded_vae_file
-    loaded_vae_file = None
-
-
-unspecified = object()
-
-
-def reload_vae_weights(sd_model=None, vae_file=unspecified):
-    from modules import lowvram, devices, sd_hijack
-
-    if not sd_model:
-        sd_model = shared.sd_model
-
-    checkpoint_info = sd_model.sd_checkpoint_info
-    checkpoint_file = checkpoint_info.filename
-
-    if vae_file == unspecified:
-        vae_file, vae_source = resolve_vae(checkpoint_file)
-    else:
-        vae_source = "from function argument"
-
-    if loaded_vae_file == vae_file:
-        return
-
-    if shared.cmd_opts.lowvram or shared.cmd_opts.medvram:
-        lowvram.send_everything_to_cpu()
-    else:
-        sd_model.to(devices.cpu)
-
-    sd_hijack.model_hijack.undo_hijack(sd_model)
-
-    load_vae(sd_model, vae_file, vae_source)
-
-    sd_hijack.model_hijack.hijack(sd_model)
-    script_callbacks.model_loaded_callback(sd_model)
-
-    if not shared.cmd_opts.lowvram and not shared.cmd_opts.medvram:
-        sd_model.to(devices.device)
-
-    print("VAE weights loaded.")
-    return sd_model
+# new code
+import shutil
+import tqdm
+from modules.ui_common import plaintext_to_html
+import safetensors.torch
+# end new code
 
 base_dir = Path(scripts.basedir())
+
+#new code
+def create_config(ckpt_result, config_source, a, b, c):
+    def config(x):
+        res = sd_models.find_checkpoint_config(x) if x else None
+        return res if res != shared.sd_default_config else None
+
+    if config_source == 0:
+        cfg = config(a) or config(b) or config(c)
+    elif config_source == 1:
+        cfg = config(b)
+    elif config_source == 2:
+        cfg = config(c)
+    else:
+        cfg = None
+
+    if cfg is None:
+        return
+
+    filename, _ = os.path.splitext(ckpt_result)
+    checkpoint_filename = filename + ".yaml"
+
+    print("Copying config:")
+    print("   from:", cfg)
+    print("     to:", checkpoint_filename)
+    shutil.copyfile(cfg, checkpoint_filename)
+
+
+checkpoint_dict_skip_on_merge = ["cond_stage_model.transformer.text_model.embeddings.position_ids"]
+#end new code
 
 def apply_checkpoint(x):
     info = modules.sd_models.get_closet_checkpoint_match(x)
@@ -338,7 +164,7 @@ def merge(weights:list, model_0, model_1, device="cpu", base_alpha=0.5, verbose=
 
     print("Restoring vae...")
 
-    vae_file = sd_vae.vae_dict.get(vae_file, None)
+    vae_file = sd_vae.get_vae_from_settings()
     sd_vae.load_vae(shared.sd_model, vae_file)
 
     print("Merge complete")
@@ -346,32 +172,21 @@ def merge(weights:list, model_0, model_1, device="cpu", base_alpha=0.5, verbose=
 
 
 class Script(scripts.Script):
-    
     def title(self):
-        return "Block Model Merge Custom"
+        return "Block Model Merge"
 
     def show(self, is_img2img):
         return True
 
-    def get_vae_options():
-        vae_options = list(sd_vae.vae_dict.keys())
-        return vae_options
-    
     def ui(self, is_img2img):
-        
-#        options = list(sd_vae.vae_dict.keys())
-#        vae_file = gr.inputs.Select(options, label='VAE File', default=options[0])
-        vae_path = os.path.abspath(os.path.join(models_path, "VAE"))
-        vae_ignore_keys = {"model_ema.decay", "model_ema.num_updates"}
-        vae_dict = {}
         gpu_merge = gr.Checkbox(label="Merge using GPU", value=True, elem_id="gpu-merge")
         verbose = gr.Checkbox(label="Verbose", value=False, elem_id="verbose-merge")
         finishreload = gr.Checkbox(label="Reload checkpoint when finished", value=False, elem_id="reload-merge")
         weights = gr.Textbox(label="Weights", lines=5, max_lines=2000, elem_id="merge-weights")
 
-        return [vae_file, gpu_merge, verbose, finishreload, weights]
+        return [gpu_merge, verbose, finishreload, weights]
 
-    def run(self, p, vae_file, gpu_merge, verbose, finishreload, weights):
+    def run(self, p, gpu_merge, verbose, finishreload, weights):
         print("Running block model merge")
         output_images = []
         infotexts = []
@@ -409,3 +224,56 @@ class Script(scripts.Script):
             modules.sd_models.load_model()
 
         return res
+
+    #adding code    
+    bake_in_vae_filename = sd_vae.vae_dict.get(bake_in_vae, None)
+    if bake_in_vae_filename is not None:
+        print(f"Baking in VAE from {bake_in_vae_filename}")
+        shared.state.textinfo = 'Baking in VAE'
+        vae_dict = sd_vae.load_vae_dict(bake_in_vae_filename, map_location='cpu')
+
+        for key in vae_dict.keys():
+            theta_0_key = 'first_stage_model.' + key
+            if theta_0_key in theta_0:
+                theta_0[theta_0_key] = to_half(vae_dict[key], save_as_half)
+
+        del vae_dict
+
+#    if save_as_half and not theta_func2:
+#        for key in theta_0.keys():
+#            theta_0[key] = to_half(theta_0[key], save_as_half)
+
+    if discard_weights:
+        regex = re.compile(discard_weights)
+        for key in list(theta_0):
+            if re.search(regex, key):
+                theta_0.pop(key, None)
+
+    ckpt_dir = shared.cmd_opts.ckpt_dir or sd_models.model_path
+
+    filename = filename_generator() if custom_name == '' else custom_name
+    filename += ".inpainting" if result_is_inpainting_model else ""
+    filename += "." + checkpoint_format
+
+    output_modelname = os.path.join(ckpt_dir, filename)
+
+    shared.state.nextjob()
+    shared.state.textinfo = "Saving"
+    print(f"Saving to {output_modelname}...")
+
+    _, extension = os.path.splitext(output_modelname)
+    if extension.lower() == ".safetensors":
+        safetensors.torch.save_file(theta_0, output_modelname, metadata={"format": "pt"})
+    else:
+        torch.save(theta_0, output_modelname)
+
+    sd_models.list_models()
+
+    create_config(output_modelname, config_source, primary_model_info, secondary_model_info, tertiary_model_info)
+
+    print(f"Checkpoint saved to {output_modelname}.")
+    shared.state.textinfo = "Checkpoint saved"
+    shared.state.end()
+
+    return [*[gr.Dropdown.update(choices=sd_models.checkpoint_tiles()) for _ in range(4)], "Checkpoint saved to " + output_modelname]
+#end new code
